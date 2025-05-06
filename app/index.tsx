28423d44a61be0ac4e677400c54b59516f664d3d
christopherweinhardt <weinhardtchristopher@gmail.com>
2025-05-06 01:52:23 -0400

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
 import { Redirect } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 
 export default function Index() {
-  return <Redirect href="/(tabs)/home" />;
+  return (
+    <Redirect href="/(tabs)/home" />
+  )
 }
